59f99986d4ffd59684f927e3a51f1e106ad9a756
SERWOFLEX <32241155+SERWOFLEX@users.noreply.github.com>
2026-03-14 19:10:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -54,8 +54,7 @@ export const boards = {
   pageSize: 256,
   timeout: 2000,
   baudRate: 115200,
-  use_8_bit_addresseses: false,
-} as Board,
+ } as Board,
 }
 
 const noop = (callback: () => void) => callback()
